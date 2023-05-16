8f2513d38f809151a1f14e9d29bc142b7deeb5cd
Minirick <duvanestivencardozo@gmail.com>
2023-05-16 12:36:23 -0500

    Api

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,30 +1,12 @@
 import React from 'react'
 import styled from '@emotion/styled'
-import ImagenMapa from "../img/mapa.jpg"
+import MapsApi from "./MapsApi"
 
 
-const Imagen = styled.img`
-    width: 50%;
-    margin: 30px 30px;
-    border-radius: 50px;
-
-    @media screen and (max-width: 1160px) {
-
-    }
-    @media screen and (max-width: 950px) {
-
-    }
-    @media screen and (max-width: 768px) {
-        width: 80%;
-    }
-
-`
-const Texto = styled.p `
-   
+const Texto = styled.h2 `   
     color: #000000;
     margin: 50px 120px;
-    font-size:20px;
-
+    font-size:30px;
     @media screen and (max-width: 1160px) {
 
     }
@@ -37,33 +19,32 @@ const Texto = styled.p `
     }
 `
 
-
-
 const Contenedor2 = styled.div`
-max-width: 100%;
-display: flex;
-gap: 10px;
-flex-wrap: wrap;
-justify-content: center;
-background-color: #dddddd;
+    max-width: 100%;
+    display: flex;
+    gap: 10px;
+    flex-wrap: wrap;
+    justify-content: center;
+    background-color: #dddddd;
+`
+
+const Contenedor3 = styled.div`
+height:800px;
 `
 
 export const Maps = () => {
   return (
 <>
+   
     <Contenedor2>
         <Texto> 
-            Es un gusto poder ayudar, estamos ubicados
-            en las pricipales ciudades del pais, ofreciendo
-            los mejores fritos de calidad.
-            orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-
+          Estamos ubicados en todo el territorio nacional
         </Texto>
-
-        <Imagen
-            src={ImagenMapa}
-        />	
     </Contenedor2>
+
+    <Contenedor3>
+        <MapsApi/>
+    </Contenedor3>
 </>
   )
 }
